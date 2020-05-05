e606c1a8bffa1d201daea70ae54f2caef1916f87
creaton60 <lovmistake@gmail.com>
2020-05-05 17:33:07 +0900

    [create] project init

---
+++ src/main/java/net/broadp/alexa/AlexaApplication.java
@@ -0,0 +1,13 @@
+package net.broadp.alexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlexaApplication.class, args);
+    }
+
+}
